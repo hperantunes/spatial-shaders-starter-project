0c4299beb0f6b34f9834c4b48df90ff08ccf0761
Hilton Perantunes <hperantunes@gmail.com>
2024-07-10 23:08:52 -0400

    Lesson 4.4

--- scripts/spatial_shaders.gd
+++ scripts/spatial_shaders.gd
@@ -1,7 +1,13 @@
 extends Node3D
 
+@onready var sphere: MeshInstance3D = $SphereExample
+
+
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
-
-
+	
+	if Input.is_action_just_pressed("set_color"):
+		var sphere_material: Material = sphere.get_active_material(0)
+		var random_color: Color = Color(randf(), randf(), randf())
+		sphere_material.set_shader_parameter("sphere_color", random_color)
