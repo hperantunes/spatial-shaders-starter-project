891ef1de48b1de61a4e66b188f3dcc1d3fde823b
Hilton Perantunes <hperantunes@gmail.com>
2024-07-21 08:58:27 -0400

    Update camera script

--- scripts/camera_controller.gd
+++ scripts/camera_controller.gd
@@ -1,39 +1,123 @@
-extends Node3D
+class_name FreeLookCamera
+extends Camera3D
 
-@export_range(1.0, 10.0) var move_speed: float = 2
-@export_range(0.001, 0.02) var lookaround_speed: float = 0.005
+# Modifier keys' speed multiplier
+const SHIFT_MULTIPLIER = 2.5
+const ALT_MULTIPLIER = 1.0 / SHIFT_MULTIPLIER
 
-var right_button_down = false
-var camera_rotation := Vector2.ZERO
+@export_range(0.0, 1.0) var sensitivity = 0.25
 
-func _process(delta):
-	if Input.is_action_just_pressed("camera_move_mode"):
-		Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	if Input.is_action_just_released("camera_move_mode"):
-		Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	
-	if Input.is_action_just_pressed("camera_speed_up"):
-		move_speed = 4
-	if Input.is_action_just_released("camera_speed_up"):
-		move_speed = 2
-	
-	if Input.is_action_pressed("camera_move_mode"):
-		if Input.is_action_pressed("move_forward"):
-			translate(Vector3.FORWARD * delta * move_speed)
-		if Input.is_action_pressed("move_backward"):
-			translate(Vector3.BACK * delta * move_speed)
-		if Input.is_action_pressed("move_left"):
-			translate(Vector3.LEFT * delta * move_speed)
-		if Input.is_action_pressed("move_right"):
-			translate(Vector3.RIGHT * delta * move_speed)
+@onready var label_esc = $VFlowContainer2/LabelEsc
+@onready var label_controls = $VFlowContainer2/LabelControls
+
+var is_label_esc_visible: bool = true
+
+# Mouse state
+var _mouse_position = Vector2(0.0, 0.0)
+var _total_pitch = 0.0
+
+# Movement state
+var _direction = Vector3(0.0, 0.0, 0.0)
+var _velocity = Vector3(0.0, 0.0, 0.0)
+var _acceleration = 30
+var _deceleration = -10
+var _vel_multiplier = 12
+
+# Keyboard state
+var _w = false
+var _s = false
+var _a = false
+var _d = false
+var _q = false
+var _e = false
+var _shift = false
+var _alt = false
+
 
 func _input(event):
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_RIGHT:
-			right_button_down = event.pressed
+	# Receives mouse motion
 	if event is InputEventMouseMotion:
-		if right_button_down == true:
-			camera_rotation += event.relative * lookaround_speed * -1.0
-			transform.basis = Basis() # Reset
-			rotate_object_local(Vector3(0, 1, 0), camera_rotation.x) # Rotate in Y
-			rotate_object_local(Vector3(1, 0, 0), camera_rotation.y) # Rotate in X
+		_mouse_position = event.relative
+	
+	# Receives mouse button input
+	if event is InputEventMouseButton:
+		match event.button_index:
+			MOUSE_BUTTON_RIGHT: # Only allows rotation if right click down
+				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
+			MOUSE_BUTTON_WHEEL_UP: # Increases max velocity
+				_vel_multiplier = clamp(_vel_multiplier * 1.1, 0.2, 20)
+			MOUSE_BUTTON_WHEEL_DOWN: # Decereases max velocity
+				_vel_multiplier = clamp(_vel_multiplier / 1.1, 0.2, 20)
+
+	# Receives key input
+	if event is InputEventKey:
+		match event.keycode:
+			KEY_W:
+				_w = event.pressed
+			KEY_S:
+				_s = event.pressed
+			KEY_A:
+				_a = event.pressed
+			KEY_D:
+				_d = event.pressed
+			KEY_Q:
+				_q = event.pressed
+			KEY_E:
+				_e = event.pressed
+
+
+# Updates mouselook and movement every frame
+func _process(delta):
+	_update_mouselook()
+	_update_movement(delta)
+	if Input.is_action_just_pressed("ui_cancel"):
+		is_label_esc_visible = not is_label_esc_visible
+		label_esc.visible = is_label_esc_visible
+		label_controls.visible = not is_label_esc_visible
+
+
+# Updates camera movement
+func _update_movement(delta):
+	# Computes desired direction from key states
+	_direction = Vector3((_d as float) - (_a as float), 
+						(_e as float) - (_q as float), 
+						(_s as float) - (_w as float))
+	
+	# Computes the change in velocity due to desired direction and "drag"
+	# The "drag" is a constant acceleration on the camera to bring it's velocity to 0
+	var offset = _direction.normalized() * _acceleration * _vel_multiplier * delta \
+		+ _velocity.normalized() * _deceleration * _vel_multiplier * delta
+	
+	# Compute modifiers' speed multiplier
+	var speed_multi = 1
+	if _shift: speed_multi *= SHIFT_MULTIPLIER
+	if _alt: speed_multi *= ALT_MULTIPLIER
+	
+	# Checks if we should bother translating the camera
+	if _direction == Vector3.ZERO and offset.length_squared() > _velocity.length_squared():
+		# Sets the velocity to 0 to prevent jittering due to imperfect deceleration
+		_velocity = Vector3.ZERO
+	else:
+		# Clamps speed to stay within maximum value (_vel_multiplier)
+		_velocity.x = clamp(_velocity.x + offset.x, -_vel_multiplier, _vel_multiplier)
+		_velocity.y = clamp(_velocity.y + offset.y, -_vel_multiplier, _vel_multiplier)
+		_velocity.z = clamp(_velocity.z + offset.z, -_vel_multiplier, _vel_multiplier)
+	
+		translate(_velocity * delta * speed_multi)
+
+
+# Updates mouse look 
+func _update_mouselook():
+	# Only rotates mouse if the mouse is captured
+	if Input.get_mouse_mode() == Input.MOUSE_MODE_CAPTURED:
+		_mouse_position *= sensitivity
+		var yaw = _mouse_position.x
+		var pitch = _mouse_position.y
+		_mouse_position = Vector2(0, 0)
+		
+		# Prevents looking up/down too far
+		pitch = clamp(pitch, -90 - _total_pitch, 90 - _total_pitch)
+		_total_pitch += pitch
+	
+		rotate_y(deg_to_rad(-yaw))
+		rotate_object_local(Vector3(1,0,0), deg_to_rad(-pitch))
